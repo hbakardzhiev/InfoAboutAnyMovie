53378fff026b206af476b05850224eeec9d3a3d0
Hristo Bakardzhiev <ch.bakardgiev@gmail.com>
2018-01-25 08:45:50 +0200

    Done

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        public static string getJson()
+        {
+            string titleOfDesiredMovie = Console.ReadLine();
+            WebClient client = new WebClient();
+            string json = client.DownloadString("http://www.omdbapi.com/?t=" + titleOfDesiredMovie + "&apikey=291bbe79");
+            return json;
+        }
+        static void Main(string[] args)
+        {
+            RootObject movie = new JavaScriptSerializer().Deserialize<RootObject>(getJson());
+            string[] array = {
+                                 "Title",
+                                 "Year",
+                                 "Rated",
+                                 "Genre",
+                                 "Writer",
+                                 "Plot"
+                             };
+            for (int i = 0; i < array.Length;i++ )
+            { 
+                var propertyInfo = movie.GetType().GetProperty(array[i]);
+                Console.WriteLine(propertyInfo.GetValue(movie).ToString());
+                Console.WriteLine();
+            }
+            var key = Console.ReadKey();
+        }
+    }
+}
